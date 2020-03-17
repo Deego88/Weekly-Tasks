c6e92a871ddd16ed0b0887cc92a1bd2fc2195969
Deego88 <60344243+Deego88@users.noreply.github.com>
2020-03-17 19:03:53 +0000

    Update Python Collatz.py
    corrections applied as per feedback

--- Python Collatz.py
+++ Python Collatz.py
@@ -1,17 +1,17 @@
-#ask user to input a positive integer
+# Richard Deegan
+# ask user to input a positive integer
 a = int(input("enter positive integer:"))
 b = 2
 
-# taking the current value and, if it is even,
-# divides it by 2
-#While a is not equal 1
+# taking the current value and, if it is even, divides it by 2, While a is not equal 1
 while  a != 1:
-#if a is even
+
+# if a is even
         if (a % b) == 0:
                 a = int(a/b)
                 print (a)
-# but if it is odd, multiply
-# it by 3 and adds
+                
+# but if it is odd, multiply it by 3 and adds
         elif (a % b) != 0:
                 a = int((a * 3 ) + 1)
                 print (a)
