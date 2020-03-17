e146ac39c80f79197df93598b9761c7bf53e6ebb
Deego88 <60344243+Deego88@users.noreply.github.com>
2020-03-17 19:07:38 +0000

    Update Python secondstring.py
    name added

--- Python secondstring.py
+++ Python secondstring.py
@@ -1,4 +1,5 @@
-#this program asks a user to input a string and outputs every second letter in reverse order
+#Ricard Deegan
+# this program asks a user to input a string and outputs every second letter in reverse order
 
 # input original string = The quick brown fox jumps over the lazy dog 
 
