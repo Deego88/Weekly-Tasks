8b055c7e861e8e29a4e24bc11dd57eaca3353e90
Deego88 <60344243+Deego88@users.noreply.github.com>
2020-03-17 18:59:29 +0000

    Add files via upload
    First attempt

---
+++ Topic-8-Plots/ipython_log.py
@@ -0,0 +1,15 @@
+# IPython log file
+
+import pands as pd
+inport pandas as pd
+import pandas as pd
+get_ipython().run_line_magic('logstart', '')
+import pandas as pd
+pd.read_csv(http://www.biostat.jhsph.edu/~rpeng/useRbook/faithful.csv)
+import pandas as pd
+pd.read_csv("http://www.biostat.jhsph.edu/~rpeng/useRbook/faithful.csv")
+df= pd.read_csv
+df
+df = pd.read_csv("http://www.biostat.jhsph.edu/~rpeng/useRbook/faithful    ...: .csv")
+df
+df = pd.read_csv("http://www.biostat.jhsph.edu/~rpeng/useRbook/faithful    ...: .csv")
